0753a18baa9460554531609d2a86a24f595134d1
tyasu106 <t.yasu106@gmail.com>
2021-04-29 08:07:54 +0900

    ♻️ export defaultの削除

--- components/Headline.js
+++ components/Headline.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Headline(props) {
+export function Headline(props) {
   console.log(props.title);
   return (
     <div>
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,18 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Links } from "../components/Links";
-import Headline from "../components/Headline";
+import { Headline } from "../components/Headline";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title>
+        Ï<title>Index Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Headline page='index' />
+        <Headline page="index" />
 
         <Links />
       </main>
