5b10c74fef8d25895f02447379930ba0b7a2e9d1
tyasu106 <t.yasu106@gmail.com>
2021-04-29 13:29:22 +0900

    💄 スタイルファイルの分割

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "./Footer.module.css";
 
 export function Footer() {
   return (
--- components/Headline.js
+++ components/Headline.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "./Headline.module.css";
 
 export function Headline(props) {
   return (
