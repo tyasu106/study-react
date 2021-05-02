585ead6a490f2a6ff15592400b8272c5247c4546
tyasu106 <t.yasu106@gmail.com>
2021-05-03 02:02:29 +0900

    🏗 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import styles from "./Main.module.css";
-import { Links } from "../components/Links";
-import { Headline } from "../components/Headline";
+import styles from "src/components/Main/Main.module.css";
+import { Headline } from "src/components/Headline";
+import { Links } from "src/components/Links";
 
 export function Main(props) {
   return (
